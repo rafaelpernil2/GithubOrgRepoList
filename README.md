393fbd3aed05f798a8ac3313510688009263a1f5
Rafael Pernil <rafaelpernil@uma.es>
2019-11-19 19:36:19 +0100

    feat: Init

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # GithubOrgRepoList
-It saves in a file the list of repositories of a Github Organisation filter using regexes
+This Node.js package saves in a file the list of repositories of a Github Organisation filtered by a property and an OR regex condition
+
+## Requirements
+
+* Node.js
+* Nothing else :P
+
+
+## Usage
+```Shell
+$ npm run start <YOUR_GITHUB_ACCESS_TOKEN> <ORGANISATION_NAME> <GITHUB_PROPERTY_NAME> <OUTPUT_FILE_PATH> <LIST_OF_REGEX_TO_FILTER_BY>
+```
+## Examples
+
+Command:
+```Shell
+$ npm run start <YOUR_GITHUB_ACCESS_TOKEN> Google name result.txt toolkit-.*-client
+```
+
+Output at ```result.txt```:
+
+```
+identity-toolkit-go-client
+identity-toolkit-java-client
+identity-toolkit-node-client
+identity-toolkit-php-client
+```
+
+#### Another example:
+
+Command:
+```Shell
+$ npm run start <YOUR_GITHUB_ACCESS_TOKEN> Google name result.txt python android angular
+```
+
+Output at ```result.txt```:
+
+```
+android-arscblamer
+android-classyshark
+android-cuttlefish
+android-emulator-container-scripts
+android-emulator-hypervisor-driver-for-amd-processors
+android-gradle-dsl
+android-kerberos-authenticator
+android-key-attestation
+android-lint-performance-probe
+android-management-api-samples
+android-studio-check
+android-uiconductor
+android-wear-stitch-script
+angular_cli
+angular_node_bind.dart
+angular-a11y-workshop
+angular-sticky-element
+blockly-android
+chatbase-python
+cloud-cup-android
+coursebuilder-android-container-module
+cpython-pt
+deputy-api-python-client
+favcolor-android
+gae-secure-scaffold-python
+gdata-python-client
+google-authenticator-android
+google-reauth-python
+google-visualization-python
+googletv-android-samples
+ground-android
+identity-toolkit-python-client
+music-synthesizer-for-android
+picview-for-android
+python_portpicker
+python-adb
+python-atfork
+python-cloud-utils
+python-fanotify
+python-fire
+python-gflags
+python-lakeside
+python-laurel
+python-spanner-orm
+python-subprocess32
+python-temescal
+python-yaml-config
+santa-tracker-android
+yara-procdump-python
+```
+
+## References
+
+See all Github Repositories properties available from Github API at https://developer.github.com/v3/repos/#response
+
+
+## License
+
+MIT
+
+## Credits
+
+Rafael Pernil Bronchalo (@rafaelpernil2) - Developer
+
---
+++ index.js
@@ -0,0 +1,185 @@
+/**
+ * Author: Rafael Pernil Bronchalo
+ */
+
+const https = require("https");
+const fs = require("fs");
+const util = require("util");
+const writeFilePromise = util.promisify(fs.writeFile);
+
+function generateORRegexString(array) {
+  let regexString = "";
+
+  for (let index = 0; index < array.length; index++) {
+    const type = array[index];
+
+    regexString = regexString + type;
+    if (index < array.length - 1) {
+      regexString = regexString + "|";
+    }
+  }
+
+  return regexString;
+}
+
+function parseLink(linkBlock) {
+  let linksArray = linkBlock;
+  linkBlock = linksArray.length == 2 ? linksArray[1] : linkBlock;
+  let parsedLinkObject = {};
+
+  linksArray = linkBlock.split(",");
+
+  for (link of linksArray) {
+    linkInfo = /<([^>]+)>;\s+rel="([^"]+)"/gi.exec(link);
+
+    const splittedURL = linkInfo[1].split("/");
+    const host = splittedURL[0] + "//" + splittedURL[2];
+    const urlWithoutHost = linkInfo[1].replace(host, "");
+    parsedLinkObject[linkInfo[2]] = urlWithoutHost;
+  }
+
+  return parsedLinkObject;
+}
+
+function httpsRequestPromise(options) {
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, res => {
+      let data = "";
+
+      res.on("data", chunk => {
+        data += chunk;
+      });
+
+      const link = res.headers.link;
+
+      res.on("end", () => {
+        const response = JSON.parse(data);
+        if (response.message === "Bad credentials") {
+          reject("Bad credentials");
+        } else {
+          resolve({ data: response, links: parseLink(link) });
+        }
+      });
+    });
+
+    req.on("error", error => {
+      reject(error);
+    });
+
+    req.end();
+  }).catch(reason => {
+    new Error(reason);
+  });
+}
+
+function orderResultsArray(array) {
+  array.sort((a, b) => {
+    return a.localeCompare(b);
+  });
+}
+
+function processConsoleInput() {
+  let processedInput;
+  const helpString = "PARAMETERS:\n" +
+    "\t1st param: GitHub Access Token\n" +
+    "\t2nd param: GitHub Organisation\n" +
+    "\t3rd param: GitHub Property to filter by\n" +
+    "\t4th param: Output file path\n" +
+    "\t5th and up params: Filter values evaluated as an or condition separated by a space \n";
+  switch (process.argv[2]) {
+    case "-h":
+      console.log(helpString);
+      break;
+    case undefined:
+      console.log(
+        "ERROR: No parameters were provided!\n" + helpString);
+      break;
+    default:
+      processedInput = {
+        token: process.argv[2],
+        orgName: process.argv[3],
+        githubPropertyName: process.argv[4],
+        outputPath: process.argv[5],
+        filterList: process.argv.splice(6)
+      };
+      break;
+  }
+
+
+
+  return processedInput;
+}
+
+async function getPropertyFromGithubRepoList(
+  token,
+  orgName,
+  githubPropertyName,
+  outputPath,
+  filterList
+) {
+  let page = 1;
+  const pageSize = 100;
+  let accData = [];
+
+  // Initial page
+  let nextPage = `/orgs/${orgName}/repos?per_page=${pageSize}&page=${page}`;
+  while (nextPage) {
+    const options = {
+      hostname: "api.github.com",
+      path: nextPage,
+      method: "GET",
+      headers: {
+        Authorization: "token " + token,
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+      }
+    };
+
+    const response = await httpsRequestPromise(options);
+    if (!response) {
+      throw new Error("Bad credentials");
+    }
+    // Get the list of repo names
+    const filteredData = response["data"].map(element => element[githubPropertyName]);
+    accData = [...accData, ...filteredData];
+
+    nextPage = response.links.next;
+    page = page + 1;
+  }
+
+  const orRegexString = generateORRegexString(filterList);
+  const regexProjectCategory = `.*(${orRegexString}).*`;
+  const regex = new RegExp(regexProjectCategory, "g");
+
+  let final = accData.filter(element => regex.test(element));
+
+  orderResultsArray(final);
+
+  await writeFilePromise(outputPath, final.join("\n"), "utf8").then(
+    () => {
+      console.log(
+        `Your list of GitHub projects from ${orgName} is ready and saved at ${outputPath}!`
+      );
+    },
+    reason => {
+      throw new Error(
+        `The file ${outputPath} could not be saved due to: ${reason}`
+      );
+    }
+  );
+}
+
+// Main
+const input = processConsoleInput();
+
+if (input) {
+  getPropertyFromGithubRepoList(
+    input.token,
+    input.orgName,
+    input.githubPropertyName,
+    input.outputPath,
+    input.filterList
+  ).catch(reason => {
+    console.error(reason);
+  });
+}
